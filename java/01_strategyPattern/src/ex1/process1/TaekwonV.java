9d0807d7c030c9a5f6a905187962d42c8a3624da
pikachu987 <pikachu987@naver.com>
2016-07-16 16:00:17 +0900

    java commit

---
+++ java/01_strategyPattern/src/ex1/process1/TaekwonV.java
@@ -0,0 +1,22 @@
+package ex1.process1;
+
+public class TaekwonV extends Robot {
+
+	public TaekwonV(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void attack() {
+		// TODO Auto-generated method stub
+		System.out.println("TaekwonV Attack");
+	}
+
+	@Override
+	public void move() {
+		// TODO Auto-generated method stub
+		System.out.println("TaekwonV Move");
+	}
+
+}
