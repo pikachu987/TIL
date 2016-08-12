b687d09de732729da8dabf78806adcf56f3227c8
pikachu987 <pikachu987@naver.com>
2016-08-12 19:56:31 +0900

    spring commit

---
+++ spring/04_PropertyEditor/01_ExtendPoint/src/main/java/com/company/ex1/Main.java
@@ -0,0 +1,13 @@
+package com.company.ex1;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class Main {
+	public static void main(String[] args) {
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:ex1_applicationContext.xml");
+		DataCollector collector1 = ctx.getBean("collector1", DataCollector.class);
+		DataCollector collector2 = ctx.getBean("collector2", DataCollector.class);
+		System.out.println(collector1.getThreshold());
+		System.out.println(collector2.getThreshold());
+	}
+}
