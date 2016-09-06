b8ecba36f21278a69594bf954f1cc3dc62c6da23
pikachu987 <pikachu987@naver.com>
2016-09-07 08:35:08 +0900

    swift commit

---
+++ swift 2.2/GoogleLogin/Pods/Target Support Files/Pods-GoogleLogin/Pods-GoogleLogin-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GoogleLogin : NSObject
+@end
+@implementation PodsDummy_Pods_GoogleLogin
+@end
---
+++ swift 2.2/KakaoFaceLogin/Pods/Target Support Files/Pods-KakaoFaceLogin/Pods-KakaoFaceLogin-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_KakaoFaceLoginVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_KakaoFaceLoginVersionString[];
+
